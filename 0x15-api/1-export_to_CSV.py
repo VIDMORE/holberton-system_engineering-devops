a57e06382f09bfd955d685f5e89a0769a86081e0
David Corredor <vidmore8@gmail.com>
2020-09-14 17:37:36 -0500

    [UPLOAD] - Tasks #1 and #2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a script that creates a CSV
+about the TO DO list progress of a user"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    user_id = argv[1]
+    filename = f'{user_id}.csv'
+    url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get("{}/users/{}" .format(url, user_id)).json()
+    todo_list = requests.get("{}/users/{}/todos" .format(url, user_id)).json()
+
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            writer.writerow([user_id, user.get('username'),
+                             task.get("completed"), task.get("title")])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a script that returns information
+"""This module defines a script that returns a json
 about the TO DO list progress of a user"""
 
+import json
 import requests
 from sys import argv
 
 if __name__ == "__main__":
 
     user_id = argv[1]
-    tasks_completed = 0
     tasks = []
+    data = {}
+    filename = f'{user_id}.json'
     url = "https://jsonplaceholder.typicode.com"
 
     user = requests.get("{}/users/{}" .format(url, user_id)).json()
     todo_list = requests.get("{}/users/{}/todos" .format(url, user_id)).json()
 
     for value in todo_list:
-        if value.get('completed') is True:
-            tasks_completed += 1
-            tasks.append(value.get('title'))
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get('name'), tasks_completed, len(todo_list)))
-    for task in tasks:
-        print("\t {}".format(task))
+        task = {}
+        task["task"] = value.get('title')
+        task["completed"] = value.get('completed')
+        task["username"] = user.get('username')
+        tasks.append(task)
+    data[user_id] = tasks
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
